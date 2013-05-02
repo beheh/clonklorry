d8f219eb40b4758c2cd1ba61b7e53496b27eea94
Benedict Etzel <developer@beheh.de>
2013-05-02 23:08:37 +0200

    added exception handling around $lorry->handle()

--- web/index.php
+++ web/index.php
@@ -1,7 +1,19 @@
 <?php
+
+//$time = microtime();
+
 error_reporting(E_ALL | E_NOTICE);
 
 require '../vendor/autoload.php';
 
-$lorry = new Lorry\Environment();
-$lorry->handle();
+try {
+	$lorry = new Lorry\Environment();
+	$lorry->handle();
+} catch(Exception $ex) {
+	header('HTTP/1.1 500 Internal Server Error');
+	header('Content-Type: text/plain');
+	echo 'An internal error occured:'.PHP_EOL;
+	echo $ex;
+}
+
+//echo (microtime() - $time).'s';
