3fa99119752f53b462af33abc74b8e997cb36b9e
Benedict Etzel <developer@beheh.de>
2013-05-03 11:10:42 +0200

    made login slightly more functional

--- src/Lorry/Presenter/Account/Login.php
+++ src/Lorry/Presenter/Account/Login.php
@@ -3,6 +3,7 @@
 namespace Lorry\Presenter\Account;
 
 use Lorry\Presenter;
+use Lorry\ModelFactory;
 
 class Login extends Presenter {
 
@@ -13,15 +14,24 @@ class Login extends Presenter {
 			return $this->redirect('/');
 		}
 
-		if(isset($_GET['by-email'])) {
+		$this->context['remember'] = true;
+		if(isset($_GET['by-email']) || isset($_COOKIE['lorry_login_email'])) {
 			$this->context['email'] = true;
 		}
+
 		$this->twig->display('account/login.twig', $this->context);
 	}
 
 	public function post() {
 		if(isset($_GET['by-email'])) {
 			$this->context['email_focus'] = true;
+			$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+			$user = ModelFactory::build('User')->byEmail($email);
+			if($user) {
+				setcookie('lorry_login_email', '1', time() + 60 * 60 * 24 * 365, '/');
+			}
+		} else {
+			setcookie('lorry_login_email', '', 0, '/');
 		}
 		return $this->get();
 	}
