9a7952d732a49faaa2c8981f453c9d6927fbf21c
Benedict Etzel <developer@beheh.de>
2013-05-04 23:07:19 +0200

    continued implementation of registration page

--- src/Lorry/Presenter/Account/Register.php
+++ src/Lorry/Presenter/Account/Register.php
@@ -6,15 +6,21 @@ use Lorry\Presenter;
 
 class Register extends Presenter {
 
+	private $context = array();
+
 	public function get() {
 		if($this->session->authenticated()) {
 			return $this->redirect('/');
 		}
-		$this->twig->display('account/register.twig');
+		$this->twig->display('account/register.twig', $this->context);
 	}
 
 	public function post() {
-		return $this->get();
+		$username = filter_input(INPUT_POST, 'username');
+		$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+		$password = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_EMAIL);
+		$password_repeat = filter_input(INPUT_POST, 'password-repeat');
+		$this->get();
 	}
 
 }
