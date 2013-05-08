8e83c15aad294d326a2019f4a51f47eb697daef9
Benedict Etzel <developer@beheh.de>
2013-05-08 11:01:21 +0200

    made addon overview presenter function again

--- src/Lorry/Presenter/Addon/Overview.php
+++ src/Lorry/Presenter/Addon/Overview.php
@@ -3,11 +3,13 @@
 namespace Lorry\Presenter\Addon;
 
 use Lorry\Presenter;
+use Lorry\ModelFactory;
+use Lorry\Exception\FileNotFoundException;
 
 class Overview extends Presenter {
 
 	public function get($game, $addon, $release = 'latest') {
-		$game = ModelFactory::build('Game')->byShort($name);
+		$game = ModelFactory::build('Game')->byShort($game);
 		if(!$game) {
 			throw new FileNotFoundException('game '.$game);
 		}
