8363c9ab159dc56be164d96e74b0209abe93607f
Benedict Etzel <developer@beheh.de>
2013-05-12 20:18:33 +0200

    added basic validation to registration form

--- src/Lorry/Presenter/Account/Register.php
+++ src/Lorry/Presenter/Account/Register.php
@@ -10,7 +10,7 @@ class Register extends Presenter {
 		if($this->session->authenticated()) {
 			return $this->redirect('/');
 		}
-		
+
 		$this->display('account/register.twig');
 	}
 
@@ -19,6 +19,31 @@ class Register extends Presenter {
 		$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 		$password = filter_input(INPUT_POST, 'password', FILTER_VALIDATE_EMAIL);
 		$password_repeat = filter_input(INPUT_POST, 'password-repeat');
+
+		$this->context['username'] = $username;
+		$this->context['email'] = $email;
+
+		$errors = array();
+
+
+		$username_length = strlen($username);
+		if(!$username || $username_length < 3 || $username_length > 16) {
+			$this->context['username_invalid'] = true;
+			$errors[] = gettext('Username invalid.');
+		}
+
+		if(!$email) {
+			$errors[] = gettext('Email address is invalid.');
+		}
+
+		if($password !== $password_repeat) {
+			$errors[] = gettext('Passwords do not match.');
+		}
+
+		if(!empty($errors)) {
+			$this->error('register', implode('<br>', $errors));
+		}
+
 		$this->get();
 	}
 
