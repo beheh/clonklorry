2fb91c8557e4db31fbe2d1279020bf81767c8aad
Benedict Etzel <developer@beheh.de>
2013-05-14 22:40:48 +0200

    updated release schema

--- src/Lorry/Model/Release.php
+++ src/Lorry/Model/Release.php
@@ -9,11 +9,36 @@ class Release extends Model {
 	public function __construct() {
 		parent::__construct('addon', array(
 			'addon' >= 'int',
-			'version' => 'string'));
+			'version' => 'string',
+			'timestamp' => 'int'));
+	}
+
+	public final function setAddon($addon) {
+		return $this->setValue('addon', $addon);
 	}
 
 	public final function byAddon($id) {
 		return $this->byValue('addon', $id);
+	}
+
+	public final function getAddon() {
+		return $this->getValue('addon');
+	}
+
+	public final function setVersion($version) {
+		return $this->setValue('version', $version);
+	}
+
+	public final function getVersion() {
+		return $this->getValue('version');
+	}
+
+	public function setTimestamp($timestamp) {
+		return $this->setValue('timestamp', $timestamp);
+	}
+
+	public function getTimestamp() {
+		return $this->getValue('timestamp');
 	}
 
 }
