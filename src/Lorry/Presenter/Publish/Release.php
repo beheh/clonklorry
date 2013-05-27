870d73694ef08389a7a52b0d8e9e9c78dc26e692
Benedict Etzel <developer@beheh.de>
2013-05-27 11:39:29 +0200

    suggest default release time

--- src/Lorry/Presenter/Publish/Release.php
+++ src/Lorry/Presenter/Publish/Release.php
@@ -3,6 +3,7 @@
 namespace Lorry\Presenter\Publish;
 
 use Lorry\Presenter;
+use DateTime;
 
 class Release extends Presenter {
 
@@ -10,6 +11,10 @@ class Release extends Presenter {
 		$this->security->requireLogin();
 		$user = $this->session->getUser();
 
+		$datetime = new DateTime('tomorrow 12:00');
+		$this->context['datetime'] =  $datetime->format('Y-m-d\TH:i:s');
+		$datetime = new DateTime();
+		$this->context['current_datetime'] = $datetime->format('Y-m-d\TH:i:s');
 		$this->display('publish/release.twig');
 	}
 
