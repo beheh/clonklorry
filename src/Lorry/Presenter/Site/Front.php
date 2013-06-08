bec60239b4b83596afc27c428e60529522b28c5f
Benedict Etzel <developer@beheh.de>
2013-06-08 12:34:00 +0200

    display new releases on front page

--- src/Lorry/Presenter/Site/Front.php
+++ src/Lorry/Presenter/Site/Front.php
@@ -3,9 +3,26 @@
 namespace Lorry\Presenter\Site;
 
 use Lorry\Presenter;
+use Lorry\ModelFactory;
 
 class Front extends Presenter {
 	public function get() {
+		$release = ModelFactory::build('Release')->all()->order('timestamp', true)->byAnything();
+		$addons = array();
+		foreach($release as $release) {
+			$addon = $release->fetchAddon();
+			$game = $addon->fetchGame();
+			$addons[] = array(
+				'title' => $addon->getTitle(),
+				'short' => $addon->getShort(),
+				'version' => $release->getVersion(),
+				'game' => array(
+					'title' => $game->getTitle(),
+					'short' => $game->getShort())
+			);
+		}
+		$this->context['new_addons'] = $addons;
+
 		$this->display('site/front.twig');
 	}
 }
