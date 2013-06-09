6f8cdd4a4a80e8ffe7df407421e764d37f6fdd9d
Benedict Etzel <developer@beheh.de>
2013-06-09 19:07:07 +0200

    added sorting to addon/game

--- src/Lorry/Presenter/Addon/Game.php
+++ src/Lorry/Presenter/Addon/Game.php
@@ -19,7 +19,25 @@ class Game extends Presenter {
 		$this->context['short'] = $game->getShort();
 
 		$query = ModelFactory::build('Addon')->all();
-		$query = $query->order('updated', true);
+
+		$sort = isset($_GET['sort']) ? $_GET['sort'] : 'date';
+		$reverse = isset($_GET['reverse']) && $_GET['reverse'] == 1 ? true : false;
+		switch($sort) {
+			case 'title':
+				$query = $query->order('title', $reverse);
+				break;
+			case 'rating':
+				//@TODO sort by rating
+				break;
+			case 'date':
+			default:
+				$sort = 'date';
+				$query = $query->order('updated', !$reverse);
+				break;
+		}
+		$this->context['sort'] = $sort;
+		$this->context['reverse'] = $reverse;
+
 		$addons = $query->byGame($game->getId());
 		$this->context['addons'] = array();
 		foreach($addons as $addon) {
