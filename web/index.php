9a39388c68ce998b15fc7a7bc22047351357dd1e
Benedict Etzel <developer@beheh.de>
2013-06-22 10:53:05 +0200

    adapted index to new environment method names

--- web/index.php
+++ web/index.php
@@ -1,19 +1,12 @@
 <?php
 
-//$time = microtime();
-
-error_reporting(E_ALL & ~(E_STRICT));
-
 require '../vendor/autoload.php';
 
 try {
 	$lorry = new Lorry\Environment();
-	$lorry->handle();
-} catch(Exception $ex) {
+	$lorry->requestHandle();
+} catch(Exception $e) {
 	header('HTTP/1.1 500 Internal Server Error');
 	header('Content-Type: text/plain');
-	echo 'An internal error occured:'.PHP_EOL;
-	echo $ex;
+	echo $e->getMessage();
 }
-
-//echo (microtime() - $time).'s';
