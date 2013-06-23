2b668ddcde6fc9015d2040c9bd4d0f33c27d05b8
Benedict Etzel <developer@beheh.de>
2013-06-23 11:48:17 +0200

    user table requires moderator status until flood protection is enabled

--- src/Lorry/Presenter/User/Table.php
+++ src/Lorry/Presenter/User/Table.php
@@ -9,6 +9,8 @@ class Table extends Presenter {
 
 	public function get() {
 
+		$this->security->requireModerator(); //@TODO rate limiting
+
 		$users = ModelFactory::build('User')->byAnything();
 		foreach($users as $user) {
 			$this->context['users'][] = $user->getUsername();
