cff87e76d47feff039df92e2cd005860431ab819
Benedict Etzel <developer@beheh.de>
2013-06-23 11:48:39 +0200

    reworked layout and workflow of addon/release publishing

--- src/Lorry/Presenter/Publish/Release.php
+++ src/Lorry/Presenter/Publish/Release.php
@@ -3,13 +3,30 @@
 namespace Lorry\Presenter\Publish;
 
 use Lorry\Presenter;
+use Lorry\ModelFactory;
+use Lorry\Exception\FileNotFoundException;
 use DateTime;
 
 class Release extends Presenter {
 
-	public function get($game, $addon, $version) {
+	public function get($gamename, $addonname, $version) {
 		$this->security->requireLogin();
-		$user = $this->session->getUser();
+
+		$game = ModelFactory::build('Game')->byShort($gamename);
+		if(!$game) {
+			throw new FileNotFoundException('game '.$gamename);
+		}
+
+		$this->context['game_short'] = $game->getShort();
+
+		$addon = ModelFactory::build('Addon')->byShort($addonname, $game->getId(), true);
+		if(!$addon) {
+			throw new FileNotFoundException('addon '.$addonname);
+		}
+
+		$this->context['addon_title'] = $addon->getTitle();
+		$this->context['addon_short'] = $addon->getShort();
+		$this->context['addon_abbreviation'] = $addon->getAbbreviation();
 
 		$datetime = new DateTime('tomorrow 12:00');
 		$this->context['datetime'] =  $datetime->format('Y-m-d\TH:i:s');
