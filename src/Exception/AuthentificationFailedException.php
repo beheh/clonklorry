3cfa6170eedf8a428360c3c79310bc7c2a192cf7
Benedict Etzel <developer@beheh.de>
2014-02-27 16:35:01 +0100

    updated exceptions

--- src/Exception/AuthentificationFailedException.php
+++ src/Exception/AuthentificationFailedException.php
@@ -5,4 +5,9 @@ namespace Lorry\Exception;
 use Lorry\Exception;
 
 class AuthentificationFailedException extends Exception {
+
+	public function getPresenter() {
+		return 'Error\AuthFailed';
+	}
+
 }
--- src/Exception/FileNotFoundException.php
+++ src/Exception/FileNotFoundException.php
@@ -5,7 +5,9 @@ namespace Lorry\Exception;
 use Lorry\Exception;
 
 class FileNotFoundException extends Exception {
+
 	public function getPresenter() {
 		return 'Error\FileNotFound';
 	}
+
 }
