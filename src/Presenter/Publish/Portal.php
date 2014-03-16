6ee381f258f0487d6dde72ddef0c078e7a2a7581
Benedict Etzel <developer@beheh.de>
2014-03-16 12:26:57 +0100

    added addon overview to publishing portal after user has created an addon

--- src/Presenter/Publish/Portal.php
+++ src/Presenter/Publish/Portal.php
@@ -29,28 +29,29 @@ class Portal extends Presenter {
 					'short' => $game->getShort(),
 					'title' => $game->getTitle(),
 				);
-			}
+		}
 
 			if(isset($_GET['for'])) {
 				$this->context['focus'] = 'title';
 			}
 
 			$addons = ModelFactory::build('Addon')->all()->byOwner($user->getId(), true);
-			$this->context['addons_unreleased'] = array();
-			$this->context['addons_released'] = array();
+			
+			$this->context['addons'] = array();
 			foreach($addons as $addon) {
 				$user_addon = array();
 				$user_addon['title'] = $addon->getTitle();
 				$user_addon['short'] = $addon->getShort();
+				$user_addon['id'] = $addon->getId();
 				$game = $addon->fetchGame();
 				if($game) {
 					$user_addon['game'] = array('title' => $game->getTitle(), 'short' => $game->getShort());
 				}
 				if(!$addon->isPublic()) {
 					$user_addon['year'] = 2013;
-					$this->context['addons_unreleased'][] = $user_addon;
+					$this->context['addons'][] = $user_addon;
 				} else {
-					$this->context['addons_released'][] = $user_addon;
+					$this->context['addons'][] = $user_addon;
 				}
 			}
 
@@ -59,44 +60,4 @@ class Portal extends Presenter {
 			$this->display('publish/greeter.twig');
 		}
 	}
-
-	public function post() {
-		$this->security->requireLogin();
-		$user = $this->session->getUser();
-
-		$errors = array();
-
-		$game_short = filter_input(INPUT_POST, 'game');
-		$game = ModelFactory::build('Game')->byShort($game_short);
-		if($game) {
-			$this->selected = $game_short;
-		} else {
-			$errors[] = gettext('Invalid game.');
-		}
-
-		$title = filter_input(INPUT_POST, 'title');
-		$short = preg_replace('#[^a-z]#', '', strtolower($title));
-		$this->context['addon_title'] = $title;
-
-		if($game) {
-			if(ModelFactory::build('Addon')->byShort($short, $game->getId())) {
-				$errors[] = gettext('Short name already exists.');
-			}
-		}
-
-		if(!$errors) {
-			$addon = ModelFactory::build('Addon');
-			$addon->setOwner($user->getId());
-			$addon->setTitle($title);
-			$addon->setShort($short);
-			$addon->setGame($game->getId());
-			$addon->save();
-
-			$this->redirect('/publish/'.$game->getShort().'/'.$addon->getShort());
-		} else {
-			$this->error('create', implode('<br>', $errors));
-			$this->get();
-		}
-	}
-
 }
