e940261cec96dde210c027a1c13cc5191b3ec3ed
Benedict Etzel <developer@beheh.de>
2014-03-17 22:58:32 +0100

    added functionality to addon edit page

--- src/Presenter/Publish/Edit.php
+++ src/Presenter/Publish/Edit.php
@@ -6,6 +6,7 @@ use Lorry\Presenter;
 use Lorry\ModelFactory;
 use Lorry\Exception\FileNotFoundException;
 use Lorry\Exception\ForbiddenException;
+use Lorry\Exception\ModelValueInvalidException;
 
 class Edit extends Presenter {
 
@@ -27,22 +28,28 @@ class Edit extends Presenter {
 
 		$this->context['title'] = $addon->getTitle();
 
-		$this->context['addontitle'] = $addon->getTitle();
+		if(!isset($this->context['addontitle'])) {
+			$this->context['addontitle'] = $addon->getTitle();
+		}
 
-		$this->context['abbreviation'] = $addon->getAbbreviation();
-		
+		if(!isset($this->context['abbreviation'])) {
+			$this->context['abbreviation'] = $addon->getAbbreviation();
+		}
+
 		if(isset($_GET['add'])) {
 			$this->context['focus_release'] = true;
 		}
-		
+
 		$games = ModelFactory::build('Game')->all()->byAnything();
 		$this->context['games'] = array();
 		foreach($games as $game) {
 			$this->context['games'][$game->getShort()] = array('title' => $game->getTitle());
 		}
 
-		$game = $addon->fetchGame();
-		$this->context['game'] = $game->getShort();
+		if(!isset($this->context['game'])) {
+			$game = $addon->fetchGame();
+			$this->context['game'] = $game->getShort();
+		}
 
 		$this->display('publish/edit.twig');
 	}
@@ -51,6 +58,48 @@ class Edit extends Presenter {
 		$this->security->requireLogin();
 
 		$addon = $this->getAddon($id);
+
+		if(isset($_GET['addon'])) {
+			$errors = array();
+
+			$title = trim(filter_input(INPUT_POST, 'title'));
+			try {
+				$addon->setTitle($title);
+				$this->context['addontitle'] = $addon->getTitle();
+			} catch(ModelValueInvalidException $ex) {
+				$errors[] = sprintf(gettext('Title is %s.'), $ex->getMessage());
+				$this->context['addontitle'] = $title;
+			}
+
+			try {
+				$game = ModelFactory::build('Game')->byShort(filter_input(INPUT_POST, 'game'));
+				if(!$game) {
+					throw new ModelValueInvalidException('invalid');
+				}
+				$this->context['game'] = $game->getShort();
+				$addon->setGame($game->getId());
+			} catch(ModelValueInvalidException $ex) {
+				$errors[] = sprintf(gettext('Game is %s.'), $ex->getMessage());
+			}
+
+			$abbreviation = trim(filter_input(INPUT_POST, 'abbreviation'));
+			try {
+				$addon->setAbbreviation($abbreviation);
+				$this->context['abbreviation'] = $addon->getAbbreviation();
+			} catch(ModelValueInvalidException $ex) {
+				$errors[] = sprintf(gettext('Abbreviation is %s.'), $ex->getMessage());
+				$this->context['abbreviation'] = $abbreviation;
+			}
+
+			if(empty($errors)) {
+				if($addon->modified()) {
+					$addon->save();
+					$this->success('addon', gettext('Addon saved.'));
+				}
+			} else {
+				$this->error('addon', implode('<br>', $errors));
+			}
+		}
 
 		$this->get($id);
 	}
