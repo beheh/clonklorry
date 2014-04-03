e5c5a27d6cc60337d7e22f51089ea641d617093a
Benedict Etzel <developer@beheh.de>
2014-04-03 17:27:09 +0200

    improve publishing portal

--- src/Presenter/Publish/Portal.php
+++ src/Presenter/Publish/Portal.php
@@ -7,8 +7,6 @@ use Lorry\ModelFactory;
 
 class Portal extends Presenter {
 
-	private $selected = '';
-
 	public function get() {
 		if($this->session->authenticated()) {
 			$this->security->requireLogin();
@@ -16,33 +14,27 @@ class Portal extends Presenter {
 
 			$games = ModelFactory::build('Game')->byAnything();
 
-			if(empty($this->selected)) {
-				if(isset($_GET['for'])) {
-					$this->selected = $_GET['for'];
-				}
-			}
-
 			$this->context['games'] = array();
 			foreach($games as $game) {
 				$this->context['games'][] = array(
-					'selected' => $game->getShort() == $this->selected,
 					'short' => $game->getShort(),
-					'title' => $game->getTitle(),
+					'title' => $game->getTitle()
 				);
-		}
+			}
 
 			if(isset($_GET['for'])) {
 				$this->context['focus'] = 'title';
 			}
 
 			$addons = ModelFactory::build('Addon')->all()->byOwner($user->getId(), true);
-			
+
 			$this->context['addons'] = array();
 			foreach($addons as $addon) {
 				$user_addon = array();
 				$user_addon['title'] = $addon->getTitle();
 				$user_addon['short'] = $addon->getShort();
 				$user_addon['id'] = $addon->getId();
+				$user_addon['public'] = $addon->isPublic();
 				$game = $addon->fetchGame();
 				if($game) {
 					$user_addon['game'] = array('title' => $game->getTitle(), 'short' => $game->getShort());
@@ -60,4 +52,5 @@ class Portal extends Presenter {
 			$this->display('publish/greeter.twig');
 		}
 	}
+
 }
