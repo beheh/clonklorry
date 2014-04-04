98d9ae10feb9fcee5b67701d216074d7a107797e
Benedict Etzel <developer@beheh.de>
2014-04-04 02:17:51 +0200

    enabled modification of version in release page

--- src/Presenter/Publish/Release.php
+++ src/Presenter/Publish/Release.php
@@ -5,6 +5,8 @@ namespace Lorry\Presenter\Publish;
 use Lorry\Presenter;
 use Lorry\ModelFactory;
 use Lorry\Exception\FileNotFoundException;
+use Lorry\Exception\ForbiddenException;
+use Lorry\Exception\ModelValueInvalidException;
 
 class Release extends Presenter {
 
@@ -36,9 +38,55 @@ class Release extends Presenter {
 		$this->context['title'] = $addon->getTitle();
 		$this->context['addonid'] = $addon->getId();
 		$this->context['version'] = $release->getVersion();
-
+		if(!isset($this->context['new_version'])) {
+			$this->context['new_version'] = $release->getVersion();
+		}
+		if(isset($_GET['version-changed'])) {
+			$this->success('version', gettext('Release saved.'));
+		}
 
 		$this->display('publish/release.twig');
+	}
+
+	public function post($id, $version) {
+		$this->security->requireLogin();
+		$this->security->requireValidState();
+
+		$addon = $this->getAddon($id);
+		$release = $this->getRelease($addon->getId(), $version);
+
+		if(isset($_POST['general-submit'])) {
+			$new_version = filter_input(INPUT_POST, 'version');
+			$this->context['new_version'] = $new_version;
+
+			$errors = array();
+
+			try {
+				$existing = ModelFactory::build('Release')->byVersion($new_version, $id);
+				if($existing && $existing->getId() != $release->getId()) {
+					$errors[] = gettext('Version already exists.');
+				}
+
+				$release->setVersion($new_version);
+			} catch(ModelValueInvalidException $ex) {
+				$errors[] = sprintf(gettext('Version is %s.'), $ex->getMessage());
+			}
+
+			if(empty($errors)) {
+				if($release->modified()) {
+					if($release->save()) {
+						$this->success('version', gettext('Version saved.'));
+						$this->redirect('/publish/addons/'.$addon->getId().'/'.$new_version.'?version-changed');
+					} else {
+						$this->error('version', gettext('Error saving the release.'));
+					}
+				}
+			} else {
+				$this->error('version', implode($errors, '<br>'));
+			}
+		}
+
+		return $this->get($id, $version);
 	}
 
 }
