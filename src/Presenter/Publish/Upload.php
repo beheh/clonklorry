b95a4f1eb0eb43060ae74962ac2dee2f99cf82ea
Benedict Etzel <developer@beheh.de>
2014-04-14 09:17:45 +0200

    change upload api to use exceptions

--- src/Presenter/Publish/Upload.php
+++ src/Presenter/Publish/Upload.php
@@ -3,7 +3,9 @@
 namespace Lorry\Presenter\Publish;
 
 use Lorry\Presenter;
-use Lorry\Exception\OutputCompleteException;
+use Lorry\Exception;
+use Lorry\Exception\ForbiddenException;
+use Lorry\Exception\FileNotFoundException;
 use Lorry\Model\User;
 
 class Upload extends Presenter {
@@ -52,7 +54,7 @@ class Upload extends Presenter {
 		// the size of the last part is between chunkSize and 2*$chunkSize
 		if($total_files * $chunk_size >= ($total_size - $chunk_size + 1)) {
 
-			// create the final destination file 
+			// create the final destination file
 			if(($fp = fopen(self::UPLOAD_DIR.'/'.$file_name, 'w')) !== false) {
 				for($i = 1; $i <= $total_files; $i++) {
 					fwrite($fp, file_get_contents($chunk_directory.'/'.$file_name.'.part'.$i));
@@ -64,42 +66,33 @@ class Upload extends Presenter {
 
 			$this->removeChunkDirectory($chunk_directory);
 		}
-		
+
 		return true;
 	}
 
 	public function get($addonid, $release) {
 		if(!$this->session->authenticated()) {
-			header('HTTP/1.1 403 Forbidden');
-			header('Content-Type: text/plain');
-			echo gettext('Forbidden');
-			throw new OutputCompleteException;
+			throw new ForbiddenException();
 		}
 
 		$user = $this->session->getUser();
 		$chunk_directory = $this->getUserDirectory($user).'/'.basename(filter_input(INPUT_GET, 'resumableIdentifier'));
 
 		$part_file = $chunk_directory.'/'.filter_input(INPUT_GET, 'resumableFilename').'.part'.filter_input(INPUT_GET, 'resumableChunkNumber', FILTER_SANITIZE_NUMBER_INT);
-		if(!file_exists($chunk_file)) {
-			header('HTTP/1.1 404 Not Found');
-			throw new OutputCompleteException;
+		if(!file_exists($part_file)) {
+			throw new FileNotFoundException();
 		}
 	}
 
 	public function post($addonid, $release) {
 		if(!$this->session->authenticated()) {
-			header('HTTP/1.1 403 Forbidden');
-			header('Content-Type: text/plain');
-			echo gettext('Forbidden');
-			throw new OutputCompleteException;
+			throw new ForbiddenException();
 		}
 
 		$file = $_FILES['file'];
 
 		if($file['error'] != 0) {
-			header('HTTP/1.1 500 Internal Server Error');
-			echo gettext('Error receiving file chunk.');
-			throw new OutputCompleteException;
+			throw new Exception('Error receiving file chunk.');
 		}
 
 		$user = $this->session->getUser();
@@ -114,14 +107,11 @@ class Upload extends Presenter {
 		$part_file = $chunk_directory.'/'.$file_name.'.part'.filter_input(INPUT_POST, 'resumableChunkNumber', FILTER_SANITIZE_NUMBER_INT);
 
 		if(!move_uploaded_file($file['tmp_name'], $part_file)) {
-			header('HTTP/1.1 500 Internal Server Error');
-			echo gettext('Error saving file chunk.');
-			throw new OutputCompleteException;
+			throw new Exception('Error saving file chunk.');
 		}
 
 		if(!$this->attemptAssembleFile($chunk_directory, $file_name, filter_input(INPUT_POST, 'resumableChunkSize', FILTER_SANITIZE_NUMBER_INT), filter_input(INPUT_POST, 'resumableTotalSize', FILTER_SANITIZE_NUMBER_INT))) {
-			echo gettext('Error assembling file.');
-			throw new OutputCompleteException;
+			throw new Exception('Error assembling file.');
 		}
 	}
 
