22e9d9e0bfd35d09914a9bb22b56cf8629b706fb
Benedict Etzel <developer@beheh.de>
2014-11-13 15:19:28 +0100

    add removal of jobs

--- src/Service/JobService.php
+++ src/Service/JobService.php
@@ -2,6 +2,7 @@
 
 namespace Lorry\Service;
 
+use Analog;
 use Resque;
 use Lorry\Exception;
 
@@ -24,20 +25,42 @@ class JobService {
 		$this->setup = true;
 	}
 	
-	public function submit($job, $args) {
-		if(!is_array($args)) {
-			throw new Exception('invalid arguments (not an array)');
-		}
-		$this->ensureSetup();
-		$class_name = '\\Lorry\\Job\\'.$job.'Job';
+	public function build($job_name) {
+		$class_name = '\\Lorry\\Job\\'.$job_name.'Job';
 		if(!class_exists($class_name)) {
 			throw new Exception('unknown job');
 		}
 		if(!is_subclass_of($class_name, '\\Lorry\\Job')) {
 			throw new Exception('job does not implement base class');			
 		}
-		$queue = call_user_func($class_name.'::getQueue');
-		return Resque::enqueue($queue, $class_name, $args);
+		return $class_name;
+	}
+	
+	public function getQueue($job_class) {
+		return call_user_func($job_class.'::getQueue');
+	}
+	
+	public function submit($job_name, $args) {
+		if(!is_array($args)) {
+			throw new Exception('invalid arguments (not an array)');
+		}
+		$this->ensureSetup();
+		$class_name = $this->build($job_name);
+		$result = Resque::enqueue($this->getQueue($class_name), $class_name, $args);
+		\Analog::debug('queuing "'.$job_name.'" (queue "'.$this->getQueue($class_name).'"): result is '.print_r($result, true));
+		return $result;
+	}
+	
+	public function remove($job_name, $args) {
+		if(!is_array($args)) {
+			throw new Exception('invalid arguments (not an array)');
+		}
+		$this->ensureSetup();
+		$class_name = $this->build($job_name);
+		$filter = array($class_name => $args);
+		$result = Resque::dequeue($this->getQueue($class_name), $filter);
+		\Analog::debug('dequeuing "'.$job_name.'" (queue "'.$this->getQueue($class_name).'"): result is '.print_r($result, true));
+		return $result;
 	}
 
 }
