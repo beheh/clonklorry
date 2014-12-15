b078b9ed17f1850cb642657da44ed7855a15c50d
Benedict Etzel <developer@beheh.de>
2014-12-15 14:17:14 +0100

    fix wrong log message in debug JobSubmitter presenter

--- src/Presenter/Debug/JobSubmitter.php
+++ src/Presenter/Debug/JobSubmitter.php
@@ -9,7 +9,7 @@ class JobSubmitter extends Presenter {
 
 	public function get() {
 		$job = filter_input(INPUT_GET, 'job');
-		Analog::info('Cleared Twig cache');
+		Analog::info('Submitting job '.$job);
 		$this->job->submit($job, array());
 
 		echo 'Job submitted.';
