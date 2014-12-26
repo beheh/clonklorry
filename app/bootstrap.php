f4c7038a507e5284d81810139b337ff6cbb73249
Benedict Etzel <developer@beheh.de>
2014-12-26 13:31:10 +0100

    fix wrong log file path in bootstrap

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,4 +2,4 @@
 
 require __DIR__.'/../vendor/autoload.php';
 
-\Analog::handler(\Analog\Handler\File::init(__DIR__.'/app/logs/lorry.log'));
+\Analog::handler(\Analog\Handler\File::init(__DIR__.'/../app/logs/lorry.log'));
