d21ccb6214e6fc79c8fa87f1b50610419dba2457
Benedict Etzel <developer@beheh.de>
2015-01-06 12:07:01 +0100

    update various urls

--- src/Presenter/Site/Clonk.php
+++ src/Presenter/Site/Clonk.php
@@ -9,6 +9,12 @@ class Clonk extends Presenter {
 	public function get() {
 		$this->session->setFlag('knows_clonk');
 		$this->context['hide_greeter'] = true;
+
+		if(isset($_GET['returnto'])) {
+			$this->redirect(filter_input(INPUT_GET, 'returnto'));
+			return;
+		}
+
 		$this->display('site/clonk.twig');
 	}
 
