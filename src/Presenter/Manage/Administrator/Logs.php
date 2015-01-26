8c301e6d9e56c8aa96649ea19c9b6c090b6e2b7e
Benedict Etzel <developer@beheh.de>
2015-01-26 16:25:06 +0100

    require and offer identification on logs page

--- src/Presenter/Manage/Administrator/Logs.php
+++ src/Presenter/Manage/Administrator/Logs.php
@@ -9,10 +9,12 @@ class Logs extends Presenter {
 
 	public function get() {
 		$this->security->requireAdministrator();
+		$this->offerIdentification();
+		$this->security->requireIdentification();
 
 		$emergency_log = Environment::PROJECT_ROOT.'/logs/emergency.log';
 		$application_log = Environment::PROJECT_ROOT.'/logs/lorry.log';
-		
+
 		$this->context['emergency_log'] = is_readable($emergency_log) ? file_get_contents($emergency_log) : 'Can\'t read file at "'.$emergency_log.'"';
 		$this->context['application_log'] = is_readable($application_log) ? file_get_contents($application_log) : 'Can\'t read file at "'.$application_log.'"';
 
