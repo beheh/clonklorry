3369be9dfbcaebf1cb7d499a10e9b15d56e9daf2
Benedict Etzel <developer@beheh.de>
2015-01-30 17:15:53 +0100

    improve removal of invalid files from release file list

--- web/resources/lorry/js/lorry-release-files.js
+++ web/resources/lorry/js/lorry-release-files.js
@@ -2,15 +2,15 @@ var releaseFileBaseUrl = base + '/api/internal/addons/' + addon + '/' + release;
 var releaseResumable = new Resumable({target: releaseFileBaseUrl + '/upload?type=data', permanentErrors: [403, 404, 415, 500, 501], query: {state: state}});
 var releaseFilesExisting = [];
 
-$(document).ready(function() {
+$(document).ready(function () {
 	$.ajax(releaseFileBaseUrl + '/query',
 			{
-				success: function(data) {
+				success: function (data) {
 					console.log(data);
 					releaseFilesExisting = data.files;
-					$(releaseFilesExisting).each(function(id, file) {
+					$(releaseFilesExisting).each(function (id, file) {
 						releaseFilesAdd(file);
-						if(file.complete) {
+						if (file.complete) {
 							releaseFilesSuccess(file);
 						}
 						else {
@@ -18,10 +18,10 @@ $(document).ready(function() {
 						}
 					});
 				},
-				error: function() {
+				error: function () {
 					$('#resumable-files-none-text').text($('#message-text-files-loading-failed').text());
 				},
-				complete: function() {
+				complete: function () {
 					updateReleaseResumableState();
 					$('#resumable-files-loading').hide();
 				}
@@ -36,11 +36,11 @@ if (!releaseResumable.support) {
 	releaseResumable.assignBrowse($('#resumable-select'));
 	releaseResumable.assignDrop($('#files'));
 	$('#resumable-pause').hide();
-	$('#resumable-upload').click(function() {
+	$('#resumable-upload').click(function () {
 		releaseResumable.upload();
 		updateReleaseResumableState();
 	});
-	$('#resumable-pause').click(function() {
+	$('#resumable-pause').click(function () {
 		releaseResumable.pause();
 		updateReleaseResumableState();
 	});
@@ -50,9 +50,9 @@ if (!releaseResumable.support) {
 	$('#resumable').show();
 }
 
-releaseResumable.on('fileAdded', function(file) {
+releaseResumable.on('fileAdded', function (file) {
 	var cancel = false;
-	$.each(releaseFilesExisting, function(key, existingFile) {
+	$.each(releaseFilesExisting, function (key, existingFile) {
 		if (file.fileName === existingFile.fileName && existingFile.progress !== -1) {
 			cancel = true;
 			return false;
@@ -69,61 +69,65 @@ releaseResumable.on('fileAdded', function(file) {
 	}
 });
 
-releaseResumable.on('fileProgress', function(file) {
+releaseResumable.on('fileProgress', function (file) {
 	if (file.isComplete())
 		return;
 	var percentage = Math.min(Math.round(file.progress() * 100), 99);
 	$('li[data-unique="' + file.uniqueIdentifier + '"] .resumable-progress').text(percentage + '%');
 });
 
-releaseResumable.on('fileSuccess', function(file) {
+releaseResumable.on('fileSuccess', function (file) {
 	releaseFilesSuccess(file);
 	updateReleaseResumableState();
 });
 
-releaseResumable.on('fileError', function(file, raw) {
+var errors = {};
+
+releaseResumable.on('fileError', function (file, raw) {
 	result = $.parseJSON(raw);
 	$('li[data-unique="' + file.uniqueIdentifier + '"] .resumable-progress').text(ucfirst(result.message));
 	$('li[data-unique="' + file.uniqueIdentifier + '"]').removeClass('list-group-item-success');
 	$('li[data-unique="' + file.uniqueIdentifier + '"]').addClass('list-group-item-danger');
+	errors[file.uniqueIdentifier] = true;
 	updateReleaseResumableState();
 });
 
 function releaseFilesAdd(file) {
 	$('li[data-filename="' + file.fileName + '"]').remove();
 	$('#resumable-files').append($('<li class="list-group-item resumable-item" data-unique="' + file.uniqueIdentifier + '" data-filename="' + file.fileName + '"><span class="resumable-filename">' + file.fileName + '</span><button type="button" class="close" title="' + $('#message-text-remove').text() + '" aria-hidden="true">&times;</button><span class="resumable-progress"></span></li>'));
-	$('li[data-unique="' + file.uniqueIdentifier + '"] .close').click(function() {
-		if ((file.isComplete && file.isComplete()) || file.complete) {
+	$('li[data-unique="' + file.uniqueIdentifier + '"] .close').click(function () {
+		var errorFile = errors[file.uniqueIdentifier];
+		if (((file.isComplete && file.isComplete()) || file.complete) && !errorFile) {
 			if (!confirm($('#message-text-confirm-remove').text())) {
 				return;
 			}
 		}
-		$.each(releaseResumable.files, function(key, clickedFile) {
+		$.each(releaseResumable.files, function (key, clickedFile) {
 			if (clickedFile.uniqueIdentifier === file.uniqueIdentifier) {
 				file.cancel();
-				return false;
+				return;
 			}
 		});
 		$.ajax(releaseFileBaseUrl + '/remove', {
 			method: 'post',
 			dataType: 'json',
 			data: {state: state, fileName: file.fileName, uniqueIdentifier: file.uniqueIdentifier}
-		})
-				.always(function(result) {
-					if (result.file === 'removed' || result.status === 404) {
-						$('li[data-unique="' + file.uniqueIdentifier + '"]').remove();
-						$.each(releaseFilesExisting, function(key, clickedFile) {
-							if (clickedFile.uniqueIdentifier === file.uniqueIdentifier) {
-								releaseFilesExisting.splice(key, 1);
-								return false;
-							}
-						});
-						updateReleaseResumableState();
-					}
-					else {
-						alert($('#message-text-remove-failed').text());
+		}).always(function (result) {
+			if (result.file === 'removed' || result.status === 404 || errorFile) {
+				$('li[data-unique="' + file.uniqueIdentifier + '"]').remove();
+				$.each(releaseFilesExisting, function (key, clickedFile) {
+					if (clickedFile.uniqueIdentifier === file.uniqueIdentifier) {
+						releaseFilesExisting.splice(key, 1);
+						return false;
 					}
 				});
+				errors[file.uniqueIdentifier] = false;
+				updateReleaseResumableState();
+			}
+			else {
+				alert($('#message-text-remove-failed').text());
+			}
+		});
 	});
 	$('li[data-unique="' + file.uniqueIdentifier + '"] .resumable-progress').text(translation.preparingUpload);
 }
@@ -163,7 +167,7 @@ function updateReleaseResumableState() {
 	}
 }
 
-$(window).on('beforeunload', function() {
+$(window).on('beforeunload', function () {
 	if (releaseResumable.isUploading()) {
 		return translation.stillUploading;
 	}
