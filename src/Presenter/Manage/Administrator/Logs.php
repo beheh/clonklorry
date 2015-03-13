c619fd08a0a1394049a523553d92c0ff1aae0789
Benedict Etzel <developer@beheh.de>
2015-03-13 15:53:07 +0100

    remove emergency.log from administration portal

--- src/Presenter/Manage/Administrator/Logs.php
+++ src/Presenter/Manage/Administrator/Logs.php
@@ -12,10 +12,8 @@ class Logs extends Presenter {
 		$this->offerIdentification();
 		$this->security->requireIdentification();
 
-		$emergency_log = Environment::PROJECT_ROOT.'/logs/emergency.log';
 		$application_log = Environment::PROJECT_ROOT.'/logs/lorry.log';
 
-		$this->context['emergency_log'] = is_readable($emergency_log) ? file_get_contents($emergency_log) : 'Can\'t read file at "'.$emergency_log.'"';
 		$this->context['application_log'] = is_readable($application_log) ? file_get_contents($application_log) : 'Can\'t read file at "'.$application_log.'"';
 
 		$this->display('manage/administrator/logs.twig');
