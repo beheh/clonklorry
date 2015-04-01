bd6135b2799ecd7ded26c7a385826024d84dd1f4
Benedict Etzel <developer@beheh.de>
2015-04-01 17:41:03 +0200

    show example titles on logged-out developer portal

--- src/Presenter/Publish/Developers.php
+++ src/Presenter/Publish/Developers.php
@@ -26,13 +26,13 @@ class Developers extends Presenter
 
         $singular_objects = array(gettext('Wipf'), gettext('Monster'), gettext('Bridge'),
             gettext('Tower'), gettext('Stage'), gettext('Pressurewave'), gettext('Fantasy'),
-            gettext('Stippel'), gettext('Quake'));
+            gettext('Stippel'), gettext('Quake'), gettext('Zap'));
         $singular_phrases = array(gettext('%s pack'), gettext('%s of despair'), gettext('%s Infinity'),
             gettext('%sarena'), gettext('%s fight'), gettext('%s race'), gettext('%s Party'),
-            gettext('%s Valley'), gettext('%s lands'), gettext('%s defense'));
+            gettext('%s lands'), gettext('%s defense'));
         $plural_objects = array(gettext('Wipfs'), gettext('Monsters'), gettext('Bridges'),
             gettext('Towers'), gettext('Pressurewaves'), gettext('Flints'), gettext('Knights'),
-            gettext('Clonks'), gettext('Stippels'));
+            gettext('Clonks'), gettext('Stippels'), gettext('Fish'));
         $plural_phrases = array(gettext('Metal & %s'), gettext('Left 2 %s'), gettext('%s descend from the top'),
             gettext('%s rise from the bottom'));
 
