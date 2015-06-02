ab1aa7ad3f860d0ccb22a95d61ee6a625c094a11
Benedict Etzel <developer@beheh.de>
2015-06-02 15:21:45 +0200

    [to squash] rework comment model

--- src/Model/Comment.php
+++ src/Model/Comment.php
@@ -2,61 +2,17 @@
 
 namespace Lorry\Model;
 
-use Lorry\Model;
+use Lorry\Model2;
 
 class Comment extends Model
 {
 
-    public function getTable()
-    {
-        return 'comment';
-    }
+    /** @ManyToOne(targetEntity="User") */
+    protected $author;
 
-    public function getSchema()
-    {
-        return array(
-            'owner' => 'int',
-            'content' => 'text',
-            'timestamp' => 'int');
-    }
+    /** @Column(type="string") */
+    protected $body;
 
-    public function setOwner($owner)
-    {
-        return $this->setValue('owner', $owner);
-    }
-
-    public function byOwner($owner)
-    {
-        return $this->byValue('owner', $owner);
-    }
-
-    public function getOwner()
-    {
-        return $this->getValue('owner');
-    }
-
-    public function fetchOwner()
-    {
-        return $this->fetch('User', 'owner');
-    }
-
-    public function setContent($content)
-    {
-        return $this->setValue('content', $content);
-    }
-
-    public function getContent()
-    {
-        return $this->getValue('content');
-    }
-
-    public function setTimestamp($timestamp)
-    {
-        return $this->setValue('timestamp', $timestamp);
-    }
-
-    public function getTimestamp()
-    {
-        return $this->getValue('timestamp');
-    }
+    /** @Column(type="datetime") */
+    protected $written;
 }
