f341a129fb865faa3003983c20e7bc84278606ab
Benedict Etzel <developer@beheh.de>
2015-06-10 23:23:37 +0200

    improve activation flow

--- src/Presenter/Account/Activate.php
+++ src/Presenter/Account/Activate.php
@@ -17,22 +17,35 @@ class Activate extends Presenter
             throw new FileNotFoundException('user '.$username);
         }
 
-        $expires = filter_input(INPUT_GET, 'expires');
-        $address = filter_input(INPUT_GET, 'address');
-
-        $hash = filter_input(INPUT_GET, 'hash');
-        if (empty($hash)) {
-            throw new BadRequestException();
+        if(!$this->session->authenticated()) {
+            $this->redirect('/login?returnto='.urlencode('/users/'.$username.'/activate?'.implode('&', array_map(function($a, $b) {
+                return $a.'='.$b;
+            }, array_keys($_GET), $_GET))));
+            return;
         }
 
-        try {
-            $expected = $this->security->signActivation($user, $expires,
-                $address);
-        } catch (\InvalidArgumentException $ex) {
-            throw new BadRequestException();
+        $this->security->requireLogin();
+
+        if($this->session->getUser() != $user) {
+            throw new ForbiddenException('attempting to activate wrong user');
         }
 
         if(!$user->isActivated()) {
+            $expires = filter_input(INPUT_GET, 'expires');
+            $address = filter_input(INPUT_GET, 'address');
+
+            $hash = filter_input(INPUT_GET, 'hash');
+            if (empty($hash)) {
+                throw new BadRequestException();
+            }
+
+            try {
+                $expected = $this->security->signActivation($user, $expires,
+                    $address);
+            } catch (\InvalidArgumentException $ex) {
+                throw new BadRequestException();
+            }
+
             if (hash_equals($expected, $hash) !== true) {
                 throw new ForbiddenException('hash does not match expected value');
             }
@@ -49,10 +62,6 @@ class Activate extends Presenter
             $this->manager->flush();
         }
 
-        if ($this->session->authenticated()) {            
-            $this->redirect('/settings?activated#contact');
-        } else {
-            $this->redirect('/login?returnto='.urlencode('/settings?activated#contact'));
-        }
+        $this->redirect('/settings?activated#contact');
     }
 }
